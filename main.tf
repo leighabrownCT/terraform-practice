30afc2220fd64d5c8f1818545299c773205417b7
leighabrownCT <leigha.brown@commercetools.com>
2022-12-17 11:11:02 -0500

    new web server

--- main.tf
+++ main.tf
@@ -11,11 +11,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0b0dcb5067f052a63"
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "terraform-Linux2"
+
+#find a specific ami image - ubuntu
+data "aws_ami" "app" {
+  most_recent = true
+
+  filter {
+
+    name = "name"
+
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  owners = ["099720109477"] #canonical owner ID
 }
+
+
+#create a new server
+resource "aws_instance" "cloudcasts_web" {
+
+  ami = data.aws_ami.app.id
+
+  instance_type = "t3.small"
+
+  root_block_device {
+    volume_size = 8
+  }
+
+}
+
